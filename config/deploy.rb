f2525c20949c2020bf79b4699069e35b80fe9c1b
alex rothenberg <alexrothenberg@alex-rothenbergs-macbook-pro.local>
2008-10-22 19:28:11 -0400

    updated cap files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 set :application, "waywework"
-set :repository,  "set your repository location here"
+set :repository,  "git://github.com/alexrothenberg/waywework.git"
+
+set :deploy_via, :remote_cache 
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -8,9 +10,11 @@ set :deploy_to, "~/apps/#{application}.patshaughnessy.net"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
 
 set :user, 'patshau'
+set :use_sudo, false
+
 
 role :app, "patshaughnessy.net"
 role :web, "patshaughnessy.net"
