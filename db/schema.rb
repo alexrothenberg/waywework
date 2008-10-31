9e4c06dc5b2de1cf2b26d28e7f96a90e11f4c64f
alex rothenberg <alexrothenberg@alex-rothenbergs-macbook-pro.local>
2008-10-30 23:07:40 -0400

    minor change to html

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081022162832) do
+ActiveRecord::Schema.define(:version => 20081031025747) do
 
   create_table "feeds", :force => true do |t|
     t.string   "url"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20081022162832) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "posts", ["published", "feed_id"], :name => "index_posts_on_published_and_feed_id"
 
 end
