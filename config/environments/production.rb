a5bcc0c0de7792e90e67b9ae46076b7a4a920036
Alex Rothenberg <alex@alexrothenberg.com>
2011-10-02 16:30:52 -0400

    compile assets in prod for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,9 @@ WayweworkIt::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # For Heroku
+  config.assets.compile = true
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
